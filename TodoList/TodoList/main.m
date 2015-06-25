b3735cd70c38e3739c8d98e6081ddc6538a61e29
mikekavouras <kavourasm@gmail.com>
2015-06-25 16:24:04 -0400

    empty project

---
+++ TodoList/TodoList/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TodoList
+//
+//  Created by Michael Kavouras on 6/25/15.
+//  Copyright (c) 2015 Mike Kavouras. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
