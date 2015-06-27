cd99c5e9ce506d2c4dd084d5bc4f24bd9e6d3408
Zoufishan Mehdi <zzmehdi@gmail.com>
2015-06-27 13:08:12 -0400

    first joint commit from Zouf's PC

--- TodoList/TodoList/main.m
+++ TodoList/TodoList/main.m
@@ -7,11 +7,67 @@
 //
 
 #import <Foundation/Foundation.h>
+//********************** INTERFACE TO DO APP *********
+@interface ToDoApp : NSObject
+
+@end
+
+//***********************  INTERFACE TODO LIST ******
+@interface ToDoList : NSObject
+
+-(void)displayItems:(NSArray*)displayItems;
+
+-(void)addItem:(NSArray*)add;
+-(NSArray *)add;
+
+-(void)removeItem:(NSArray*)remove;
+-(NSArray *)remove;
+
+-(void)markItem:(NSArray*)mark;
+-(NSArray *)mark;
+
+-(void)listCompletedItems;
+
+@end
+
+//************************INTERFACE TODO ITEM
+@interface ToDoItem : NSObject
+
+-(void)setItemName:(NSString *)itemName;
+-(NSString *)itemName;
+
+-(int)setPriority:(int)priority;
+
+@end
+
+
+//*************** IMPLEM TO DO APP ***************
+//@implementation
+//@end
+
+
+
+//*************** IMPLEM TO DO LIST ***************
+@implementation ToDoList {
+    NSString *_name;
+    NSMutableArray *_listOfItems;
+}
+@end
+
+
+
+//*************** IMPLEM TO DO ITEM ***************
+@implementation ToDoItem {
+    NSString *_itemName;
+    NSInteger priority;
+}
+@end
+
+
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
     }
     return 0;
 }
